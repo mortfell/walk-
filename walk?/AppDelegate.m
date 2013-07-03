f42c47e7343cb86d96785688f301d70d9c47ea3e
mortfell <mortfelloktorium@gmail.com>
2013-07-03 17:30:34 -0500

    Initial Commit

---
+++ walk?/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  walk?
+//
+//  Created by J. Riley Hill on 13-07-03.
+//  Copyright (c) 2013 Mortfell Oktorium. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ walk?/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  walk?
+//
+//  Created by J. Riley Hill on 13-07-03.
+//  Copyright (c) 2013 Mortfell Oktorium. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
